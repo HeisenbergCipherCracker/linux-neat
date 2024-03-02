99898e46b7f975ab686d199e36aac69e9a327afa
HeisenbergCipherCracker <alimirmohammad.1386@gmail.com>
2024-03-02 12:02:44 -0500

    Some missing packages

--- scripts/kconfig/pkg_conf.h
+++ scripts/kconfig/pkg_conf.h
@@ -2,30 +2,12 @@
 #define REPO_GET_H
 #include <stdio.h>
 #include <string.h>
-
-#include <stdio.h>
-#include <string.h>
-#include <curl/curl.h>
-
-// Callback function to write data to a file
-size_t write_callback(void *ptr, size_t size, size_t nmemb, FILE *stream) {
-    return fwrite(ptr, size, nmemb, stream);
-}
+#include <stdlib.h>
 
 void download_repo(const char* url_repo) {
-    CURL *curl;
-    FILE *output_file;
-
-    curl = curl_easy_init();
-    if (curl) {
-        output_file = fopen("repo.tar.gz", "wb");
-        curl_easy_setopt(curl, CURLOPT_URL, url_repo);
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, output_file);
-        curl_easy_perform(curl);
-        curl_easy_cleanup(curl);
-        fclose(output_file);
-    }
+    char command[100];
+    sprintf(command, "wget %s -O repo.tar.gz", url_repo);
+    system(command);
 }
 
 char* repo_init_sqlgo_keyword() {
@@ -48,6 +30,5 @@ char* repo_init_sqlgo_keyword() {
     fclose(file);
     return NULL;
 }
-
 
 #endif
